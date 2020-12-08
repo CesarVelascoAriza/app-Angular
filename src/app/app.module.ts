8c54425d9b47647564470b73cda5aa10e554a06b
Cesar <cesar@localhost.localdomain>
2020-12-07 22:40:12 -0500

    primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavegacionComponent } from './components/navegacion/navegacion.component';
+import { UsuariosComponent } from './components/usuarios/usuarios.component';
+import { RolesComponent } from './components/roles/roles.component';
+import { PresentacionComponent } from './components/presentacion/presentacion.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavegacionComponent,
+    UsuariosComponent,
+    RolesComponent,
+    PresentacionComponent
   ],
   imports: [
     BrowserModule,
