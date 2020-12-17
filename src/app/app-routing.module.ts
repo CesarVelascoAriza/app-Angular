5f5c9feab4b8800d638f948ae67f7cd7386da47e
Cesar <cesar@localhost.localdomain>
2020-12-16 21:50:45 -0500

    materialaize

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PresentacionComponent } from './components/presentacion/presentacion.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: '', component: PresentacionComponent
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
